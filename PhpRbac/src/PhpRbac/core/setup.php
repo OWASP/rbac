be6814ce1a0dc744202f102ca784dfde36eae774
jburns131 <github@jbwebware.com>
2013-08-29 23:32:35 -0400

    Issue #10: Renamed GetSQLs, InstallPDOMySQL, InstallPDOSQLite and InstallMySQLi

--- PhpRbac/src/PhpRbac/core/setup.php
+++ PhpRbac/src/PhpRbac/core/setup.php
@@ -9,7 +9,7 @@ if ($adapter=="pdo_mysql")
 	catch (PDOException $e)
 	{
 		if ($e->getCode()==1049) //database not found
-			InstallPDOMySQL($host,$user,$pass,$dbname);
+			installPdoMysql($host,$user,$pass,$dbname);
 		else
 			throw $e;
 	}
@@ -17,7 +17,7 @@ if ($adapter=="pdo_mysql")
 elseif ($adapter=="pdo_sqlite")
 {
 	if (!file_exists($dbname))
-		InstallPDOSQLite($host,$user,$pass,$dbname);
+		installPdoSqlite($host,$user,$pass,$dbname);
 	else
 		Jf::$Db=new PDO("sqlite:{$dbname}",$user,$pass);
 // 		Jf::$Db=new PDO("sqlite::memory:",$user,$pass);
@@ -26,17 +26,17 @@ else # default to mysqli
 {
 	Jf::$Db=new mysqli($host,$user,$pass,$dbname);
 	if(Jf::$Db->connect_errno==1049);
-		InstallMySQLi($host,$user,$pass,$dbname);
+		installMysqli($host,$user,$pass,$dbname);
 }
-function GetSQLs($dbms)
+function getSqls($dbms)
 {
 	$sql=file_get_contents(__DIR__."/sql/{$dbms}.sql");
 	$sql=str_replace("PREFIX_",Jf::tablePrefix(),$sql);
 	return explode(";",$sql);
 }
-function InstallPDOMySQL($host,$user,$pass,$dbname)
+function installPdoMysql($host,$user,$pass,$dbname)
 {
-	$sqls=GetSQLs("mysql");
+	$sqls=getSqls("mysql");
 	$db=new PDO("mysql:host={$host};",$user,$pass);
 	$db->query("CREATE DATABASE {$dbname}");
 	$db->query("USE {$dbname}");
@@ -46,18 +46,18 @@ function InstallPDOMySQL($host,$user,$pass,$dbname)
 	Jf::$Db=new PDO("mysql:host={$host};dbname={$dbname}",$user,$pass);
 	Jf::$RBAC->reset(true);
 }
-function InstallPDOSQLite($host,$user,$pass,$dbname)
+function installPdoSqlite($host,$user,$pass,$dbname)
 {
 	Jf::$Db=new PDO("sqlite:{$dbname}",$user,$pass);
-	$sqls=GetSQLs("sqlite");
+	$sqls=getSqls("sqlite");
 	if (is_array($sqls))
 		foreach ($sqls as $query)
 		Jf::$Db->query($query);
 	Jf::$RBAC->reset(true);
 }
-function InstallMySQLi($host,$user,$pass,$dbname)
+function installMysqli($host,$user,$pass,$dbname)
 {
-	$sqls=GetSQLs("mysql");
+	$sqls=getSqls("mysql");
 	$db=new mysqli($host,$user,$pass);
 	$db->query("CREATE DATABASE {$dbname}");
 	$db->select_db($dbname);
